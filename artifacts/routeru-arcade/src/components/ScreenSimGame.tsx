49ddb56f7bdd65e7ca80a74beda84525be5e0c61
RouterU <derrick.jimenez@usfoods.com>
2026-04-30 17:04:46 -0500

    Update ScreenSimGame.tsx

--- artifacts/routeru-arcade/src/components/ScreenSimGame.tsx
+++ artifacts/routeru-arcade/src/components/ScreenSimGame.tsx
@@ -31,7 +31,7 @@ interface ScreenSimGameProps {
 }
 
 const MAX_WRONG_CLICKS = 3;
-const SHOW_HOTSPOT_DEBUG = true;
+const SHOW_HOTSPOT_DEBUG = false;
 
 const QUESTIONS: ScreenSimQuestion[] = [
   {
